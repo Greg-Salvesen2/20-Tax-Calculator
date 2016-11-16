6070e1fa906f505dc4a01d2d461405e121515a07
Greg Salvesen <greg@latenightdesigns.com>
2016-11-15 21:05:21 -0500

    Added functionality to calculate tax

--- 20 Tax Calculator/main.cpp
+++ 20 Tax Calculator/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "This program calculates your total, including sales tax." << std::endl;
+    float itemCost = 0.0;
+    float taxPercent = 0.0;
+    
+    // Ask the user for the value of the items and the tax percentage
+    std::cout << "Enter the value of your item(s): ";
+    std::cin >> itemCost;
+    std::cout << "Enter the tax percentage: ";
+    std::cin >> taxPercent;
+    
+    // Calculate the tax and add the tax to the item cost in order to get the total
+    float tax = floor((itemCost * (taxPercent / 100)) * 100 + 0.5) / 100;
+    float total = itemCost + tax;
+    
+    std::cout << total << std::endl;
+    
     return 0;
 }
